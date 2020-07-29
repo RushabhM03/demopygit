96c69772ef88dad2879b5d1b86864eebfdf09c6c
RushabhM03 <rushabh.maru123@gmail.com>
2020-07-29 15:01:12 +0530

    final

--- samp.py
+++ samp.py
@@ -1,21 +1,29 @@
 # prime
 
 class prime:
+
   def __init__(self):
     self.n = 0
+
   def inp(self):
-    self.n = int(input("enter a number")
-  def find(self):
-    b = True
+    self.n = int(input("enter a number"))
+
+  def check(self):
+    flag = True
     for i in range(2,self.n):
-      if(self.n%i==0):
-        b = False
+      if(self.n&i==0):
+        flag = False
         break
-    return b
-ob = prime()
-ob.inp()
-b = ob.find()
-if(b == True):
-  print("primt")
-else:
-  print("not prime")
+    return flag
+
+def main():
+  ob = prime()
+  ob.inp()
+  b = ob.check()
+
+  if(b == True):
+    print("prime")
+  else:
+    print("not prime")
+
+main()
